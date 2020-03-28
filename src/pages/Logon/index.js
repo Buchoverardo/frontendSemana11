8fd1ea67df5c624f120e90bb8deb6017a6e55e6c
wellington verardo <wellingtonverardo@MacBook-Pro-de-wellington.local>
2020-03-28 02:30:32 -0300

    subindo paginas novas

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-
-import { Link } from 'react-router-dom';
-
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../services/api';
 
 import './styles.css';
 
@@ -10,15 +10,41 @@ import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+
+  const [id, setId] = useState('');
+
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const res = await api.post('session', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', res.data.name);
+
+      history.push('/profile');
+
+    } catch {
+      alert('Falha no login, tente novamente')
+    }
+
+  }
+
   return(
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="logo"/>
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça o Login</h1>
 
-          <input placeholder="Sua ID"/>
+          <input 
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className="button" type="submit">Entrar</button>
           <Link to="/register" className="back-link">
             <FiLogIn size={16} color="#e02041"/>
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,14 +1,45 @@
-import React from 'react';
-
-import { Link } from 'react-router-dom';
-
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function Register() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [city, setCity] = useState('');
+  const [uf, setUf] = useState('');
+
+  const history = useHistory();
+
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    };
+
+    try {
+      const res = await api.post('ongs', data);
+
+      alert(`Seu ID de acesso: ${res.data.id}`);
+      history.push('/');
+    } catch (err) {
+      alert(`Erro no cadastro, tente novamente`);
+    }
+  }
+
   return (
     <div className="register-container">
       <div className="content">
@@ -22,18 +53,40 @@ export default function Register() {
 
           <Link to="/" className="back-link">
             <FiArrowLeft size={16} color="#e02041"/>
-            Não tenho cadastro
+            Fazer Login
           </Link>
 
         </section>
-        <form>
-          <input placeholder="Nome da ONG" />
-          <input type="email" placeholder="Email" />
-          <input placeholder="Whatsapp" />
+        <form onSubmit={handleRegister}>
+          <input 
+            placeholder="Nome da ONG" 
+            value={name}
+            onChange={e => setName(e.target.value)}  
+          />
+          <input 
+            type="email" 
+            placeholder="Email" 
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input 
+            placeholder="Whatsapp" 
+            value={whatsapp}
+            onChange={e => setWhatsapp(e.target.value)}
+          />
 
           <div className="input-group">
-            <input placeholder="cidade" />
-            <input placeholder="uf" style={{width: 80 }} />
+            <input 
+              placeholder="cidade" 
+              value={city}
+              onChange={e => setCity(e.target.value)}
+            />
+            <input 
+              placeholder="uf" 
+              value={uf}
+              onChange={e => setUf(e.target.value)}
+              style={{width: 80 }} 
+            />
           </div>
 
           <button className="button" type="submit">Cadastrar</button>
